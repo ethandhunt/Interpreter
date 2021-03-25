f57932c65db739979a204fe4b47249446390d659
ethandhunt <46333321+ethandhunt@users.noreply.github.com>
2021-03-26 07:49:25 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 def Tokenise(string):
     return string.split(">")
 
@@ -17,13 +19,18 @@ def run(ins):
     if ins[0] == "+":
         for thing in ins[1:].split("+"):
             pointer += 1
+            try:
+                errorQuery = mem[pointer]
+            except:
+                mem[pointer] = 0
     if ins[0] == "-":
         for thing in ins[1:].split("-"):
             pointer -= 1
-    try:
-        errorQuery = mem[pointer]
-    except:
-        mem[pointer] = 0
+            try:
+                errorQuery = mem[pointer]
+            except:
+                mem[pointer] = 0
+    
 
     # Goto Functionality
     if ins[0] == "!":
@@ -33,16 +40,24 @@ def run(ins):
     if ins[0] == "*":
         RAA[ins[1:]] = mem[pointer]
 
-    # Moving pointer using constant
+    # Moving pointer using integer constants
     if ins[0] == "@":
         pointer = int(ins[1:])
     
     # Changing memory values with integer constants
     if ins[0] == "$":
         mem[pointer] = int(ins[1:])
+    
+    # Incrementing memory values with integer constants
+    if ins[0] == "i":
+        mem[pointer] += int(ins[1:])
+    
+    # Adding delays in microseconds
+    if ins[0] == "#":
+        time.sleep(int(ins[1:]) / 100)
 
     # Ending
-    print(f"{instruction}: {ins} {pointer}")
+    print(f"{instruction}: {ins} {pointer}: {mem[pointer]}")
 
 
 string = input("Enter Code: ")
