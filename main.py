4993439d8786de14394aa3f5fb07fc2b6543c83b
ethandhunt <46333321+ethandhunt@users.noreply.github.com>
2021-03-26 13:52:21 +1300

    A lot of stuff

--- main.py
+++ main.py
@@ -4,6 +4,25 @@ def Tokenise(string):
     return string.split(">")
 
 
+def prnt(thing):
+    if do_print:
+        print(thing)
+
+
+def RAAval(string):
+    if string[1] == "R":
+        return RAAval(string[1:])
+    return RAA[string[1:]]
+
+
+def value(string):
+    if string[0] == "R":
+        return RAAval(string)
+    try:
+        return int(string)
+    except:
+        return string
+
 def run(ins):
     global pointer
     global instruction
@@ -34,38 +53,60 @@ def run(ins):
 
     # Goto Functionality
     if ins[0] == "!":
-        instruction = int(ins[1:])
+        instruction = value(ins[1:])
     
     # Reading memory into RAA
     if ins[0] == "*":
-        RAA[ins[1:]] = mem[pointer]
+        RAA[value(ins[1:])] = mem[pointer]
 
-    # Moving pointer using integer constants
+    # Moving pointer using integer constants and RAA
     if ins[0] == "@":
-        pointer = int(ins[1:])
+        pointer = int(value(ins[1:]))
+        try:
+            errorQuery = mem[pointer]
+        except:
+            mem[pointer] = 0
     
-    # Changing memory values with integer constants
+    # Setting memory values with integer constants and RAA
     if ins[0] == "$":
-        mem[pointer] = int(ins[1:])
+        mem[pointer] = value(ins[1:])
     
-    # Incrementing memory values with integer constants
+    # Incrementing memory values with integer constants and RAA
     if ins[0] == "i":
-        mem[pointer] += int(ins[1:])
+        mem[pointer] += int(value(ins[1:]))
     
-    # Adding delays in microseconds
+    # Adding delays in milliseconds(w/ RAA)
     if ins[0] == "#":
-        time.sleep(int(ins[1:]) / 100)
+        time.sleep(value(ins[1:]) / 1000)
+    
+    # Conditional Jumps
+    if ins[0] == "?":
+        if mem[pointer] == value(ins[1:].split(':')[0]):
+            instruction = value(ins[1:].split(':')[1])
+    
+    # Print
+    if ins[0] == ".":
+        print(mem[pointer])
+    
+    # Adding memory and RAA values into a RAA value
+    if ins[0] == "%":
+        if type(value(ins[1:].split(':')[0])) == int:
+            RAA[ins[1:].split(':')[1]] += mem[value(ins[1:].split(':')[0])]
+        else:
+            RAA[ins[1:].split(':')[1]] += value(ins[1:].split(':')[0])
 
     # Ending
-    print(f"{instruction}: {ins} {pointer}: {mem[pointer]}")
+    prnt(f"{instruction}: {ins} {pointer}: {mem[pointer]}")
 
 
+do_print = input("Enable Debug Mode? y/n: ") == "y"
 string = input("Enter Code: ")
 list = Tokenise(string)
 pointer = 0
 instruction = 0
-mem = {}
+mem = {0: 0}
 RAA = {}
 while instruction < len(list):
     run(list[instruction])
-    print(mem)
+    prnt(f"mem:{mem}")
+    prnt(f"RAA:{RAA}")
